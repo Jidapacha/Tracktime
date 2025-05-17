31bf511c3c5e630a5670ffd8790b9661380930ec
Jidapacha <s6404062663029@email.kmutnb.ac.th>
2025-05-17 10:35:11 +0700

    ...

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log("=== DEBUG ENV ===");
+console.log("REACT_APP_SUPABASE_URL", process.env.REACT_APP_SUPABASE_URL);
+console.log("REACT_APP_SUPABASE_ANON_KEY", process.env.REACT_APP_SUPABASE_ANON_KEY);
+
+
 const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const SUPABASE_SERVICE_KEY = process.env.REACT_APP_SUPABASE_SERVICE_KEY;
@@ -7,7 +12,3 @@ const SUPABASE_SERVICE_KEY = process.env.REACT_APP_SUPABASE_SERVICE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);      
 export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY); 
-
-console.log("URL =", process.env.REACT_APP_SUPABASE_URL);
-console.log("KEY =", process.env.REACT_APP_SUPABASE_ANON_KEY);
-
