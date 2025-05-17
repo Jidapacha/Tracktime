21cfea60864634783917e0501594b27db46abf07
Jidapacha <s6404062663029@email.kmutnb.ac.th>
2025-05-17 10:19:20 +0700

    ....

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,5 +4,5 @@ const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const SUPABASE_SERVICE_KEY = process.env.REACT_APP_SUPABASE_SERVICE_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);      
+export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY); 
